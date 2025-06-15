21255dc0f56249da98b08d131fdada3b0cd655cd
Joao Prestes <50972527+PrestesJoo@users.noreply.github.com>
2025-06-15 19:49:55 +0000

    finaliza o /status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,36 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("Select 1 +1 as soma");
-  console.log(result.rows);
-  response.status(200).json({ retorno: "OK do João" });
+  const updateAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+  const databaseConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseConnectionsValue =
+    databaseConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenConnectionsResult = await database.query({
+    text: "SELECt count(*)::int FROM pg_stat_activity WHERE datname= $1;",
+    values: [databaseName],
+  });
+
+  const databaseOpenedConnectionsValue =
+    databaseOpenConnectionsResult.rows[0].count;
+  console.log(databaseOpenedConnectionsValue);
+
+  response.status(200).json({
+    update_at: updateAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseConnectionsValue),
+        opened_connections: databaseOpenedConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
